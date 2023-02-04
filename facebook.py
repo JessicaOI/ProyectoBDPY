635da9c187066812df1bd8a768edc02aa75aa106
JessicaOI <70552512+JessicaOI@users.noreply.github.com>
2023-02-03 20:11:59 -0600

    probado login y sign up

---
+++ facebook.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, redirect, render_template, session
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+collection = db["users"]
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return redirect('/welcome')
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = collection.find_one({'username': username})
+        if user and user['password'] == password:
+            session['username'] = username
+            return redirect('/welcome')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        collection.insert_one({'username': username, 'password': password})
+        return redirect('/login')
+    return render_template('signup.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
