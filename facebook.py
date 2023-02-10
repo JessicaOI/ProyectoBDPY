addf61ae580656ad6811d6145f77d894a1daa2b3
JessicaOI <70552512+JessicaOI@users.noreply.github.com>
2023-02-10 16:54:09 -0600

    al fin arregle lo que se chingo

--- facebook.py
+++ facebook.py
@@ -1,37 +1,46 @@
 from flask import Flask, request, redirect, render_template, session
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
+
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-collection = db["users"]
+db = client["proyecto1"]
+collectionUsers = db["users"]
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['usernameLogin']
+        email = request.form['emailLogin']
         password = request.form['passwordLogin']
-        print(username,password)
-        user = collection.find_one({'username': username})
+        print(email,password)
+        user = collectionUsers.find_one({'email': email})
         if user and user['password'] == password:
-            session['username'] = username
+            session['email'] = email
             return redirect('/welcome')
     return render_template('signupLogin.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def signup():
-    if 'username' in session:
+    if request.method == 'POST':
+        name = request.form['name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form.get("gender")
+        
+        if not gender:
+            return "You must select a gender option."
+        
+        collectionUsers.insert_one({'name': name, 'last_name': last_name,'email': email, 'password': password, 'gender': gender})
         return redirect('/welcome')
-    else:
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            collection.insert_one({'username': username, 'password': password})
-            return redirect('/login')
     return render_template('signupLogin.html')
+
+
+
 
 @app.route('/welcome')
 def welcome():
@@ -39,7 +48,7 @@ def welcome():
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
+    session.pop('email', None)
     return redirect('/')
 
 if __name__ == '__main__':
